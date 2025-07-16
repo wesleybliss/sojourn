4152b585823978e28e9a8b0ec9fb1f164d15f177
Wesley Bliss <wesley.bliss@gmail.com>
2025-07-16 15:23:33 -0500

    WIP scripts

--- src/bin/db/addTeamMember.js
+++ src/bin/db/addTeamMember.js
@@ -1,9 +1,38 @@
 import 'dotenv/config'
+import path from 'node:path'
 import db from '../../db2/index.js'
 import * as schemas from '../../db2/schema.js'
+import { eq } from 'drizzle-orm'
+
+const args = process.argv.slice(2)
+const [teamId, email] = args
+
+const script = path.basename(process.argv[0])
+const usage = `USAGE: ${script} <team-ID> <email>`
 
 const main = async () => {
     
+    if (!teamId?.length)
+        throw new Error('Invalid team ID')
+    
+    if (!email?.length || !email?.includes('@'))
+        throw new Error('Invalid email')
+    
+    const team = await db
+        .select()
+        .from(schemas.teams)
+        .where(eq(schemas.teams.id, teamId))
+    
+    if (!team)
+        throw new Error('Team not found')
+    
+    const user = await db
+        .select()
+        .from(schemas.users)
+        .where(eq(schemas.users.email, email))
+    
+    if (!user)
+        throw new Error('User not found')
     
     
 }
@@ -11,6 +40,8 @@ const main = async () => {
 main()
     .then(() => process.exit(0))
     .catch(e => {
+        console.log({ args, teamId, email })
+        console.log(usage)
         console.error(e)
         process.exit(1)
     })
